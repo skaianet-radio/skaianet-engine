4c2531a578598fb58120681ef77ab627283da574
Kitty Esty <ki@tty.sh>
2015-01-13 22:29:11 -0500

    added GPL3 license info to scripts

--- ices.py
+++ ices.py
@@ -1,9 +1,30 @@
+###
+# Copyright (c) 2015, George Burfeind (Kitty)
+# All rights reserved.
+#
+# This file is part of skaianet-engine.
+#
+# skaianet-engine is free software: you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the 
+# License, or (at your option) any later version.
+# 
+# skaianet-engine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with skaianet-engine.  If not, see <http://www.gnu.org/licenses/>.
+###
+
 from string import *
-import config
 import sys
 import os
+os.chdir(os.path.dirname(sys.argv[0]))
 import datetime
 import mysql.connector
+import config
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 
